7322b0e009eb2df62386435024f1b367d6d518dd
Hamza <hamza2024code@example.com>
2026-07-15 19:02:36 +0100

    Add temporary show_error_log.php to help user diagnose server 500 error

---
+++ public/show_error_log.php
@@ -0,0 +1,10 @@
+<?php
+header('Content-Type: text/plain; charset=utf-8');
+$logFile = __DIR__ . '/../storage/logs/laravel.log';
+if (file_exists($logFile)) {
+    $lines = file($logFile);
+    $lastLines = array_slice($lines, -60);
+    echo implode("", $lastLines);
+} else {
+    echo "Log file not found at: " . $logFile;
+}
