d0b591b4f9bc0f93ba469be9b15555a36a7283ca
Hamza <hamza2024code@example.com>
2026-07-16 11:33:06 +0100

    Cleanup check_repo.php diagnostic script

--- public/pull.php
+++ public/pull.php
@@ -78,7 +78,6 @@ $files = [
     'resources/views/admin/modules/juries_index.blade.php' => 'https://raw.githubusercontent.com/Hamza2024-CODE/sig/main/resources/views/admin/modules/juries_index.blade.php',
     'resources/views/layouts/main.blade.php' => 'https://raw.githubusercontent.com/Hamza2024-CODE/sig/main/resources/views/layouts/main.blade.php',
     'app/Domains/Academic/Repositories/ApprenantRepository.php' => 'https://raw.githubusercontent.com/Hamza2024-CODE/sig/main/app/Domains/Academic/Repositories/ApprenantRepository.php',
-    'public/check_repo.php' => 'https://raw.githubusercontent.com/Hamza2024-CODE/sig/main/public/check_repo.php',
     'routes/web.php' => 'https://raw.githubusercontent.com/Hamza2024-CODE/sig/main/routes/web.php',
     'public/pull.php' => 'https://raw.githubusercontent.com/Hamza2024-CODE/sig/main/public/pull.php'
 ];
