e5ee87aac531ac550f8b00d310e86639ee199862
Hamza <hamza2024code@example.com>
2026-07-16 11:48:37 +0100

    Diag: Add view_logs.php diagnostic script

--- public/pull.php
+++ public/pull.php
@@ -79,6 +79,7 @@ $files = [
     'resources/views/layouts/main.blade.php' => 'https://raw.githubusercontent.com/Hamza2024-CODE/sig/main/resources/views/layouts/main.blade.php',
     'app/Domains/Academic/Repositories/ApprenantRepository.php' => 'https://raw.githubusercontent.com/Hamza2024-CODE/sig/main/app/Domains/Academic/Repositories/ApprenantRepository.php',
     'public/debug_absences.php' => 'https://raw.githubusercontent.com/Hamza2024-CODE/sig/main/public/debug_absences.php',
+    'public/view_logs.php' => 'https://raw.githubusercontent.com/Hamza2024-CODE/sig/main/public/view_logs.php',
     'public/check_section_schema.php' => 'https://raw.githubusercontent.com/Hamza2024-CODE/sig/main/public/check_section_schema.php',
     'routes/web.php' => 'https://raw.githubusercontent.com/Hamza2024-CODE/sig/main/routes/web.php',
     'public/pull.php' => 'https://raw.githubusercontent.com/Hamza2024-CODE/sig/main/public/pull.php'
--- public/view_logs.php
+++ public/view_logs.php
@@ -1,12 +1,10 @@
 <?php
-// Secure view logs utility
 header('Content-Type: text/plain; charset=utf-8');
-
 $logFile = __DIR__ . '/../storage/logs/laravel.log';
 if (file_exists($logFile)) {
     $lines = file($logFile);
-    $lastLines = array_slice($lines, -100);
+    $lastLines = array_slice($lines, -50);
     echo implode("", $lastLines);
 } else {
-    echo "Laravel log file not found at " . $logFile;
+    echo "Log file not found!\n";
 }
