2dcd0c1c353e3430a18fa25adf380cda3145501a
Hamza <hamza2024code@example.com>
2026-07-19 09:49:18 +0100

    Remove swap_bacha_taj.php and check_hfsql_data.php from pull.php list

--- public/pull.php
+++ public/pull.php
@@ -59,8 +59,6 @@ $files = [
     'app/Domains/Academic/Repositories/OffresRepository.php' => 'https://raw.githubusercontent.com/Hamza2024-CODE/sig/main/app/Domains/Academic/Repositories/OffresRepository.php',
     'app/Domains/Academic/Services/OffresService.php' => 'https://raw.githubusercontent.com/Hamza2024-CODE/sig/main/app/Domains/Academic/Services/OffresService.php',
     'public/report_private_offers.php' => 'https://raw.githubusercontent.com/Hamza2024-CODE/sig/main/public/report_private_offers.php',
-    'public/swap_bacha_taj.php' => 'https://raw.githubusercontent.com/Hamza2024-CODE/sig/main/public/swap_bacha_taj.php',
-    'public/check_hfsql_data.php' => 'https://raw.githubusercontent.com/Hamza2024-CODE/sig/main/public/check_hfsql_data.php',
     'app/Http/Controllers/Admin/ModulesController.php' => 'https://raw.githubusercontent.com/Hamza2024-CODE/sig/main/app/Http/Controllers/Admin/ModulesController.php'
 ];
 
