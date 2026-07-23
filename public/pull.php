2ae22a3d853380d01c152cfa65a50f189b69bf6e
Hamza <hamza2024code@example.com>
2026-07-23 14:33:32 +0100

    fix(pull): add UtilisateursController.php to files array in pull.php

--- public/pull.php
+++ public/pull.php
@@ -73,7 +73,8 @@ $files = [
     'resources/views/admin/espace-employe/index.blade.php' => 'https://raw.githubusercontent.com/Hamza2024-CODE/sig/main/resources/views/admin/espace-employe/index.blade.php',
     'resources/views/auth/login.blade.php' => 'https://raw.githubusercontent.com/Hamza2024-CODE/sig/main/resources/views/auth/login.blade.php',
     'app/Http/Controllers/Auth/LoginController.php' => 'https://raw.githubusercontent.com/Hamza2024-CODE/sig/main/app/Http/Controllers/Auth/LoginController.php',
-    'resources/views/admin/users/credentials.blade.php' => 'https://raw.githubusercontent.com/Hamza2024-CODE/sig/main/resources/views/admin/users/credentials.blade.php'
+    'resources/views/admin/users/credentials.blade.php' => 'https://raw.githubusercontent.com/Hamza2024-CODE/sig/main/resources/views/admin/users/credentials.blade.php',
+    'app/Http/Controllers/Admin/UtilisateursController.php' => 'https://raw.githubusercontent.com/Hamza2024-CODE/sig/main/app/Http/Controllers/Admin/UtilisateursController.php'
 ];
 
 $makeWritable = function($path) {
